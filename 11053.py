79b95f25c9779ee1727928a019b3b085562d1300
hseoky desktop <skdlzl753@naver.com>
2024-06-10 13:12:00 +0900

    24.06.10

---
+++ 11053.py
@@ -0,0 +1,8 @@
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [1] * n
+for i in range(1, n):
+    for j in range(0, i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
