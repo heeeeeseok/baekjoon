4b1001250e1dffab02498c9b27a2584abf6c8baf
hseoky desktop <skdlzl753@naver.com>
2024-06-11 19:40:02 +0900

    24.06.11

---
+++ 1806.py
@@ -0,0 +1,29 @@
+def solve():
+    left = 0
+    right = 0
+    cur_sum = 0
+    while cur_sum < s and right < n:
+        cur_sum += nums[right]
+        right += 1
+    if cur_sum < s:  # 모두 더해도 S보다 작은 경우
+        print(0)
+        return
+
+    answer = right - left  # 현재 S를 넘는 부분합의 길이
+    while left <= right and left < n:
+        if cur_sum < s and right < n:
+            cur_sum += nums[right]
+            right += 1
+        else:
+            cur_sum -= nums[left]
+            left += 1
+        if cur_sum >= s:
+            answer = min(answer, right - left)
+    print(answer)
+    return
+
+
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+solve()
+
